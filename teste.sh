5ba72b3e51ba7948b2c49ed0ed001f51277a48a5
RicardoMazur <ricardmmazur@gmail.com>
2022-03-31 21:00:44 -0300

    Teste.sh

---
+++ teste.sh
@@ -0,0 +1,11 @@
+echo Oi meu nome é Ricardo Mazur
+echo Que dia é hoje ?
+date;
+echo Estamos atualmente localizados no folder
+pwd;
+echo O usurario conectado na maquina é:
+whoami;
+echo Vamos ver se conseguimos nos conectar com o Google ?
+ping www.google.com;
+echo Todos os arquivos listados 
+ls;
